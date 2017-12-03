57672098a3c32077587e2d3d0555f1d20beed03d
Faisal Hasnain <faisal@faisalhasnain.com>
2017-12-04 00:22:29 +0500

    Added schema for Transactions

---
+++ imports/api/transactions/collection.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import BaseSchema from '../../utils/base-schema';
+
+export const Transactions = new Mongo.Collection('transactions');
+
+Transactions.schema = new SimpleSchema({
+  description: {
+    type: String,
+    unique: true
+  },
+  type: {
+    type: String,
+    allowedValues: ['Debit', 'Credit']
+  },
+  amount: {
+    type: Number
+  }
+}).extend(BaseSchema);
+
+Transactions.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; }
+});
+
+Transactions.attachSchema(Transactions.schema);
