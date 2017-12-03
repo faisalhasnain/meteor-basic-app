8623664a7604716b667331d517a015e04155ce15
Faisal Hasnain <faisal@faisalhasnain.com>
2017-12-04 00:50:27 +0500

    Added pages for transactions

---
+++ imports/ui/pages/transactions.js
@@ -0,0 +1,33 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Transactions } from '/imports/api/transactions/collection';
+import { showConfirm } from '/imports/ui/components/confirm.js';
+import './transactions.html';
+
+Template.transactions.onCreated(function () {
+  this.subscribe('transactions', {});
+});
+
+Template.transactions.helpers({
+  transactions() {
+    return Transactions.find({}, { sort: { title: 1 } });
+  }
+});
+
+Template.transactions.events({
+  'click #delete-platform': async (event, templateInstance) => {
+    event.preventDefault();
+    const { transaction } = templateInstance.data;
+    const ans = await showConfirm({
+      title: 'Confirm Delete?',
+      message: `Do you really want to delete ${transaction.title} from transactions?`
+    });
+    if (!ans) return;
+    try {
+      const removed = await Meteor.callAsync('Transactions.remove', transaction._id);
+      console.log('Transaction Deleted', removed);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
